c81802750ff58aebb9869613c409947144b4b3d2
Eric Richard <reric0802@gmail.com>
2025-01-26 16:56:53 +0300

    adde coe to ensure passing

--- server/app.py
+++ server/app.py
@@ -9,6 +9,7 @@ from models import db, Pet
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -16,16 +17,47 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return make_response(
-        '<h1>Welcome to the pet directory!</h1>',
-        200
-    )
+    body = {'message': 'Welcome to the pet directory!'}
+    return make_response(body, 200)
 
 
 @app.route('/demo_json')
 def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+    pet = Pet.query.first()
+    pet_dict = {'id': pet.id,
+            'name': pet.name,
+            'species': pet.species
+            }
+    return make_response(pet_dict, 200)
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter_by(id=id).first()
+    if pet:
+        body = {'id': pet.id,
+                'name': pet.name,
+                'species': pet.species
+                }
+        return make_response(body, 200)
+    else:
+        body = {'message': f'Pet {id} not found'}
+        return make_response(body, 404)
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets =Pet.query.filter_by(species=species).all()
+    pet_arr = []
+    if pets:
+        for pet in pets:
+            pet_dict = {'id': pet.id,
+                        'name': pet.name,}
+            pet_arr.append(pet_dict)
+        body = {'count': len(pet_arr),
+                'pets': pet_arr}
+        return make_response(body, 200)
+    else:
+        body = {'message': f'No pets found for species {species}'}
+        return make_response(body, 404)
 
 
 if __name__ == '__main__':
